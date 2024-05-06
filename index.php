6268cb03652647d3a97a943260d5114a3c1076a4
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-06 16:55:01 +0200

    [add] print Giocattoli

--- index.php
+++ index.php
@@ -56,6 +56,31 @@ $accessories = array_filter($products, fn ($product) => get_class($product) === 
       </tbody>
     </table>
 
+    <h2>Giocattoli</h2>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Cat.</th>
+          <th scope="col">Immagine</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Prezzo</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <?php foreach ($toys as $item) : ?>
+          <tr>
+            <td><?php echo $item->category->icon ?></td>
+            <td><img class="w-25" src="<?php echo $item->image ?>" alt="<?php echo $item->name ?>"></td>
+            <td><?php echo $item->name ?></td>
+            <td>&euro; <?php echo $item->getFormatPrice() ?></td>
+          </tr>
+        <?php endforeach; ?>
+
+      </tbody>
+    </table>
+
+
 
 
   </div>
