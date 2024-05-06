f3034b6f8ab43644a93daad2156462eb8e1d5233
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-06 16:56:19 +0200

    [add] print accessori

--- index.php
+++ index.php
@@ -80,7 +80,29 @@ $accessories = array_filter($products, fn ($product) => get_class($product) === 
       </tbody>
     </table>
 
+    <h2>Accessori</h2>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Cat.</th>
+          <th scope="col">Immagine</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Prezzo</th>
+        </tr>
+      </thead>
+      <tbody>
 
+        <?php foreach ($accessories as $item) : ?>
+          <tr>
+            <td><?php echo $item->category->icon ?></td>
+            <td><img class="w-25" src="<?php echo $item->image ?>" alt="<?php echo $item->name ?>"></td>
+            <td><?php echo $item->name ?></td>
+            <td>&euro; <?php echo $item->getFormatPrice() ?></td>
+          </tr>
+        <?php endforeach; ?>
+
+      </tbody>
+    </table>
 
 
   </div>
